dc66ae77914c9fd6e05fc54d77f9de253fe8b873
JacopoTrincanato <trincanatoj@gmail.com>
2025-02-20 16:42:39 +0100

    fix bugs adding for cycle

--- java-inheritance-shop/src/org/lessons/java/Carrello.java
+++ java-inheritance-shop/src/org/lessons/java/Carrello.java
@@ -1,7 +1,8 @@
+//fai sempre l'override del metodo toString per stampare in maniera chiara le informazioni
+
 package org.lessons.java;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Carrello {
@@ -10,122 +11,103 @@ public class Carrello {
         // creo uno scanner
         Scanner scanner = new Scanner(System.in);
 
-        // mostro a schermo la domanda iniziale
-        System.out.println("Vuoi inserire un nuovo smartphone, un nuovo televisore o un nuovo paio di cuffie?");
-
-        // creo una variabile che permetta all'utente di scegliere l'opzione
-        String opzione = scanner.nextLine();
-
         // creo un arrayList carrello dove salvare i prodotti
-        ArrayList<Prodotto> carrello = new ArrayList<Prodotto>();
+        Prodotto[] carrello = new Prodotto[3];
 
-        // controllo che tipo di opzione ha scelto il cliente e in base a quello stampo
-        // il modello di inserimento dati
-        switch (opzione) {
+        // ciclo all'interno del carrello per aggiungere gli elementi
+        for (int i = 0; i < carrello.length; i++) {
 
-            case "smartphone":
+            // mostro a schermo la domanda iniziale
+            System.out.println("Vuoi inserire un nuovo smartphone, un nuovo televisore o un nuovo paio di cuffie?");
 
-                // creo le variabili con i dati da inserire
-                System.out.println("Inserisci il nome");
-                String nomeTelefono = scanner.nextLine();
+            // creo una variabile che permetta all'utente di scegliere l'opzione
+            String opzione = scanner.nextLine();
 
-                System.out.println("Inserisci la marca");
-                String marcaTelefono = scanner.nextLine();
+            // creo le variabili con i dati della classe prodotto da inserire
+            System.out.println("Inserisci il nome");
+            String nome = scanner.nextLine();
 
-                System.out.println("Inserisci il prezzo");
-                BigDecimal prezzoTelefono = scanner.nextBigDecimal();
+            System.out.println("Inserisci la marca");
+            String marca = scanner.nextLine();
 
-                System.out.println("Inserisci l'iva");
-                BigDecimal ivaTelefono = scanner.nextBigDecimal();
-                scanner.nextLine();
+            System.out.println("Inserisci il prezzo");
+            BigDecimal prezzo = scanner.nextBigDecimal();
 
-                System.out.println("Inserisci l'imeiCode");
-                String imeiCode = scanner.nextLine();
+            System.out.println("Inserisci l'iva");
+            BigDecimal iva = scanner.nextBigDecimal();
+            scanner.nextLine();
 
-                System.out.println("Inserisci la memoria");
-                int memoria = scanner.nextInt();
+            // controllo che tipo di opzione ha scelto il cliente e in base a quello stampo
+            // il modello di inserimento dati
+            switch (opzione) {
 
-                Smartphone smartphone = new Smartphone(nomeTelefono, marcaTelefono, prezzoTelefono, ivaTelefono,
-                        imeiCode, memoria);
+                case "smartphone":
 
-                System.out.println(smartphone);
+                    // creo le variabili con i dati della classe Smartphone da inserire
+                    System.out.println("Inserisci l'imeiCode");
+                    String imeiCode = scanner.nextLine();
 
-                // aggiungo l'ultimo prodotto alla fine
-                carrello.add(smartphone);
+                    System.out.println("Inserisci la memoria");
+                    int memoria = scanner.nextInt();
+                    scanner.nextLine();
 
-                break;
+                    Smartphone smartphone = new Smartphone(nome, marca, prezzo, iva,
+                            imeiCode, memoria);
 
-            case "televisore":
+                    // indico che lo smartphone è uguale ad un elemento del carrello
+                    carrello[i] = smartphone;
 
-                // creo le variabili con i dati da inserire
-                System.out.println("Inserisci il nome");
-                String nomeTelevisore = scanner.nextLine();
+                    break;
 
-                System.out.println("Inserisci la marca");
-                String marcaTelevisore = scanner.nextLine();
+                case "televisore":
 
-                System.out.println("Inserisci il prezzo");
-                BigDecimal prezzoTelevisore = scanner.nextBigDecimal();
+                    // creo le variabili con i dati della classe Televisori da inserire
+                    System.out.println("Inserisci se è smart scegliendo tra true e false");
+                    boolean isSmart = scanner.nextBoolean();
 
-                System.out.println("Inserisci l'iva");
-                BigDecimal ivaTelevisore = scanner.nextBigDecimal();
-                scanner.nextLine();
+                    System.out.println("Inserisci la dimensioni");
+                    int dimensioni = scanner.nextInt();
+                    scanner.nextLine();
 
-                System.out.println("Inserisci se è smart scegliendo tra true e false");
-                boolean isSmart = scanner.nextBoolean();
+                    Televisori televisore = new Televisori(nome, marca, prezzo, iva,
+                            isSmart, dimensioni);
 
-                System.out.println("Inserisci la dimensioni");
-                int dimensioni = scanner.nextInt();
+                    // indico che il televisore è uguale ad un elemento del carrello
+                    carrello[i] = televisore;
 
-                Televisori televisore = new Televisori(nomeTelevisore, marcaTelevisore, prezzoTelevisore, ivaTelevisore,
-                        isSmart, dimensioni);
+                    break;
 
-                System.out.println(televisore);
+                case "cuffie":
 
-                // aggiungo l'ultimo prodotto alla fine
-                carrello.add(televisore);
+                    // creo le variabili con i dati della classe Cuffie da inserire
+                    System.out.println("Inserisci il colore");
+                    String coloreCuffie = scanner.nextLine();
 
-                break;
+                    System.out.println("Inserisci se sono wireless o meno scegliendo tra true e false");
+                    boolean isWireless = scanner.nextBoolean();
 
-            case "cuffie":
+                    System.out.println("Inserisci se sono cablate o meno scegliendo tra true e false");
+                    boolean isCablate = scanner.nextBoolean();
+                    scanner.nextLine();
 
-                // creo le variabili con i dati da inserire
-                System.out.println("Inserisci il nome");
-                String nomeCuffie = scanner.nextLine();
+                    Cuffie cuffie = new Cuffie(nome, marca, prezzo, iva,
+                            coloreCuffie, isWireless, isCablate);
 
-                System.out.println("Inserisci la marca");
-                String marcaCuffie = scanner.nextLine();
+                    // indico che le cuffie sono uguali ad un elemento del carrello
+                    carrello[i] = cuffie;
 
-                System.out.println("Inserisci il prezzo");
-                BigDecimal prezzoCuffie = scanner.nextBigDecimal();
+                    break;
+            }
 
-                System.out.println("Inserisci l'iva");
-                BigDecimal ivaCuffie = scanner.nextBigDecimal();
-                scanner.nextLine();
-
-                System.out.println("Inserisci il colore");
-                String coloreCuffie = scanner.nextLine();
-
-                System.out.println("Inserisci se sono wireless o meno scegliendo tra true e false");
-                boolean isWireless = scanner.nextBoolean();
-
-                System.out.println("Inserisci se sono cablate o meno scegliendo tra true e false");
-                boolean isCablate = scanner.nextBoolean();
-
-                Cuffie cuffie = new Cuffie(nomeCuffie, marcaCuffie, prezzoCuffie, ivaCuffie,
-                        coloreCuffie, isWireless, isCablate);
-
-                System.out.println(cuffie);
-
-                // aggiungo l'ultimo prodotto alla fine
-                carrello.add(cuffie);
-
-                break;
         }
-
-        System.out.println(carrello);
 
         // chiudo lo scanner
         scanner.close();
+
+        // ciclo all'interno del carrello per stampare gli elementi
+        for (int i = 0; i < carrello.length; i++) {
+            System.out.println(carrello[i]);
+        }
+
     }
 }
